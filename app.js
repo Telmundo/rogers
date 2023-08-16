ce2a36ebf05171189dc5e018238c0b8291891fe0
Telmundo <142410018+Telmundo@users.noreply.github.com>
2023-08-16 08:27:13 -0400

    Update app.js

--- app.js
+++ app.js
@@ -67,7 +67,7 @@ document.getElementById('form').addEventListener('submit', function (event) {
     };
 
     // Send the form data via Email.js
-    emailjs.send("service_gq7an17", "template_xe7frwd", formData)
+    emailjs.send("service_yotr4yo", "template_ve0zy5v", formData)
         .then(function (response) {
             console.log('SUCCESS!', response.status, response.text);
             redirectToURL();
